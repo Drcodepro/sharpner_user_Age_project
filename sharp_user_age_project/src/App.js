384b37028ddf928215b7e1607b673bc1e0dd8c28
Drcodepro <drrahangdale01@gmail.com>
2024-02-29 18:39:22 +0530

    actual functionality added in the project.

--- sharp_user_age_project/src/App.js
+++ sharp_user_age_project/src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react';
 import './App.css';
+import InputForm from './component/form_details/InputForm.js';
+import UserList from './component/User_List_components/UserList.js';
+
+let userData = [{ name: "raja", age: 21 }];
 
 function App() {
-  return (
   
-  );
+  const [Data, setData] = useState(userData);
+
+  const changeState=(newData)=>{
+    setData((prevState) => {
+      let updatedData=[...prevState];
+      updatedData.unshift(newData)
+      return updatedData;
+    })
+  };
+
+  return (<div className='main_container'>
+    <InputForm changeState={changeState} />
+    <UserList userData={Data} />
+  </div>);
 }
 
 export default App;
