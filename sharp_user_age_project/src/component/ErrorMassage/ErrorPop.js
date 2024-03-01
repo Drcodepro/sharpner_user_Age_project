fd38126d90a4281eaea1ebfbdcb089f06ddf29e9
Drcodepro <drrahangdale01@gmail.com>
2024-03-01 20:59:16 +0530

    error pop up using React Portals...

--- sharp_user_age_project/src/component/ErrorMassage/ErrorPop.js
+++ sharp_user_age_project/src/component/ErrorMassage/ErrorPop.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './ErrorPop.css';
 import Button from '../form_details/Button';
 
 function ErrorPop(props) {
-    return (<>
+    
+    return ReactDOM.createPortal(<>
     <div className='backDrop' onClick={props.errorPopHandle}/>
         <div className='errorPopBox'>
             <h2>Error</h2>
             <p>{props.errormsg} </p>
             <div className='errorButton'><Button onClick={props.errorPopHandle}>ok</Button></div>
         </div>
-        </>
+        </>,
+        document.getElementById("popUp")
     )
 }
 export default ErrorPop;
